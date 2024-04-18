ef026ad550b03c39364bf847ed23ee596202aaf5
Leticia Halana Ribeiro de Sousa <leticia.halana.tr@gmail.com>
2024-04-17 22:16:58 -0300

    feat: js para criação da contagem de likes

--- scripts/like.js
+++ scripts/like.js
@@ -1,20 +1,30 @@
+
 const coracao_desativado = document.querySelectorAll("#cinza");
 const coracao_ativado = document.querySelectorAll("#vermelho");
+let like = document.querySelectorAll(".like_number");
 
 for (let i = 0; i < coracao_desativado.length; i++) {
 
     coracao_desativado[i].addEventListener("click", () => {
-        coracao_ativado[i].classList.toggle("desativado");
-        coracao_desativado[i].classList.toggle("ativado");
-        coracao_ativado[i].classList.toggle("ativado");
-        coracao_desativado[i].classList.toggle("desativado");
+
+        let cont = parseInt(like[i].innerHTML) + 1;
+        like[i].innerHTML = cont;
+
+        coracao_ativado[i].classList.remove("desativado");
+        coracao_desativado[i].classList.remove("ativado");
+        coracao_ativado[i].classList.add("ativado");
+        coracao_desativado[i].classList.add("desativado");
 
     });
 
     coracao_ativado[i].addEventListener("click", () => {
-        coracao_ativado[i].classList.toggle("ativado");
-        coracao_desativado[i].classList.toggle("desativado");
+        let cont = parseInt(like[i].innerHTML) - 1;
+        like[i].innerHTML = cont;
+        coracao_ativado[i].classList.remove("ativado");
+        coracao_desativado[i].classList.remove("desativado");
         coracao_ativado[i].classList.add("desativado");
         coracao_desativado[i].classList.add("ativado");
     })
+
 }
+
