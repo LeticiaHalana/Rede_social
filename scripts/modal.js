fcdae33b15fbf460df59a814453bbac3cde70541
Leticia Halana Ribeiro de Sousa <leticia.halana.tr@gmail.com>
2024-04-19 13:19:52 -0300

    feat: finalização do modal, e da versão desktop

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,18 +1,20 @@
 const dialog = document.querySelector('dialog');
 const modal = document.querySelectorAll(".modal");
 const button = document.querySelectorAll(".abrir_post");
-const fechar = document.getElementById("fechar");
+const button_fechar = document.createElement("button");
+button_fechar.classList.add("fechar");
 
 
 for (let i = 0; i < modal.length; i++) {
     button[i].addEventListener("click", () => {
-        dialog.innerHTML+= modal[i].innerHTML;
+
+        dialog.innerHTML = modal[i].innerHTML;
+        dialog.appendChild(button_fechar);
         dialog.showModal();
     })
-fechar.addEventListener("click", () => {
-        dialog.style.display = "none";
+
+    button_fechar.addEventListener("click", () => {
+        console.log(123);
+        dialog.close();
     })
-   
-    
 }
- 
